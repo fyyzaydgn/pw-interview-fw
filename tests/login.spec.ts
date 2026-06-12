d10f5d30688b5d09ead0ae484c9ba3f84748f13b
Feyyaz Aydogan <feyyaz.aydogan.nl@hotmail.com>
2026-06-12 14:54:39 +0200

    test(login): cover positive and negative login scenarios

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('https://the-internet-5chk.onrender.com/login');
+});
+
+test.afterEach(async ({ page }) => {
+    console.log('Test completed')
+});
+
+test('valid login', async ({ page }) => {
     await page.locator('input[name="username"]').click();
     await page.locator('input[name="username"]').fill('tomsmith');
     await page.locator('input[name="password"]').click();
     await page.locator('input[name="password"]').fill('SuperSecretPassword');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('You logged into a secure area')).toBeVisible();
+});
+
+test('invalid login', async ({ page }) => {
+    await page.locator('input[name="username"]').click();
+    await page.locator('input[name="username"]').fill('xxx');
+    await page.locator('input[name="password"]').click();
+    await page.locator('input[name="password"]').fill('xxx');
+    await page.getByRole('button', { name: 'Login' }).click();
+    // await expect(page.getByText('You logged into a secure area')).toBeVisible();
 });
