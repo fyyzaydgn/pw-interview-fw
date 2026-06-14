e67b82b2cb0c43cd8dc1fbaf67439872f5ec5ffe
Feyyaz Aydogan <feyyaz.aydogan.nl@hotmail.com>
2026-06-14 18:50:25 +0200

    chore:add smoke annotation for smoke tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,7 +8,7 @@ test.afterEach(async ({ page }) => {
     console.log('Test completed')
 });
 
-test('valid login', async ({ page }) => {
+test('@validlogin @smoke valid login', async ({ page }) => {
     await page.locator('input[name="username"]').click();
     await page.locator('input[name="username"]').fill('tomsmith');
     await page.locator('input[name="password"]').click();
@@ -17,7 +17,7 @@ test('valid login', async ({ page }) => {
     await expect(page.getByText('You logged into a secure area')).toBeVisible();
 });
 
-test('invalid login', async ({ page }) => {
+test('@invalidlogin @smoke invalid login', async ({ page }) => {
     await page.locator('input[name="username"]').click();
     await page.locator('input[name="username"]').fill('xxx');
     await page.locator('input[name="password"]').click();
